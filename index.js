0fcd54cde03b420cc4d2c8391426f27145f98226
Jonathan Lee <jonathanleeee@gmail.com>
2018-02-09 04:06:44 +0000

    disaplaying our todos correctly, connecting the form to our API, making the delete button work

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ var todoRoutes = require("./routes/todos");
     
 app.use(bodyParser.json());    
 app.use(bodyParser.urlencoded({extedned: true}));   
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
     
 app.get("/", function(req, res) {
-   res.send("Hello from the root"); 
+   res.sendFile("index.html"); 
 });    
 
 app.use('/api/todos', todoRoutes);
