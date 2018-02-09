67496dcb5e2f3e4800be20df78a9b7a103dcba22
Jonathan Lee <jonathanleeee@gmail.com>
2018-02-09 06:45:46 +0000

    make the delete button work, toggling todo completion

--- public/app.js
+++ public/app.js
@@ -8,8 +8,13 @@ $(document).ready(function() {
           createTodo();
       } 
    });
+  
+  $(".list").on("click", "li", function(){
+     updateTodo($(this));
+  }); 
    
-  $(".list").on("click", "span", function(){
+  $(".list").on("click", "span", function(e){
+    e.stopPropagation();
     removeTodo($(this).parent());
   });
 });
@@ -24,6 +29,7 @@ function addTodos(todos) {
 function addTodo(todo) {
    var newTodo = $('<li class="task">'+ todo.name + '<span>X</span></li>');
    newTodo.data("id", todo._id);
+   newTodo.data("completed", todo.completed);
    if(todo.completed) {
        newTodo.addClass("done");
    }
@@ -56,5 +62,21 @@ function removeTodo(todo) {
     })
     .catch(function(err) {
         console.log(err);
+    });
+}
+
+function updateTodo(todo){
+    var updateUrl = "/api/todos/" + todo.data("id");
+    var isDone = todo.data("completed");
+    var updateData = {completed: !isDone};
+    $.ajax({
+        method: "PUT",
+        url: updateUrl,
+        data: updateData
+    })
+    .then(function(updatedTodo){
+        todo.toggleClass("done");
+        todo.data("completed", isDone);
+        console.log(updateData);
     });
 }
